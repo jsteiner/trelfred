24c8aeb637d3d2b261af1fdc34c00624452385d0
Josh Steiner <josh@jsteiner.me>
2016-06-19 18:29:12 -0400

    Set the board icon based on the background color

--- src/Trelfred/Board.hs
+++ src/Trelfred/Board.hs
@@ -2,29 +2,43 @@ module Trelfred.Board
     ( Board (..)
     ) where
 
-import Control.Monad (mzero)
+import Data.Monoid ((<>))
 
 import Data.Aeson
 import qualified Data.Text as T
+import qualified Data.HashMap.Strict as Map
 
 data Board = Board
     { boardId :: T.Text
     , boardName :: T.Text
     , boardUrl :: T.Text
+    , boardColor :: Maybe T.Text
     } deriving (Show)
 
 instance FromJSON Board where
-    parseJSON (Object b) =
-        Board
-        <$> b .: "id"
-        <*> b .: "name"
-        <*> b .: "url"
-    parseJSON _ = mzero
+    parseJSON = withObject "board" $ \b -> do
+        bid <- b .: "id"
+        name <- b .: "name"
+        url <- b .: "url"
+        prefs <- b .: "prefs"
+        color <- prefs .:? "backgroundColor"
+        pure $ Board bid name url color
 
 instance ToJSON Board where
-    toJSON (Board bid name url) =
+    toJSON (Board bid name url color) =
         object
             [ "uid" .= bid
             , "title" .= name
             , "arg" .= url
+            , "icon" .= toIcon color
             ]
+
+toIcon :: Maybe T.Text -> Map.HashMap T.Text T.Text
+toIcon color = Map.fromList [ ("path", toIconPath color) ]
+
+toIconPath :: Maybe T.Text -> T.Text
+toIconPath (Just color) = "icons/" <> T.drop 1 color <> ".png"
+toIconPath Nothing = toIconPath $ Just defaultColor
+
+defaultColor :: T.Text
+defaultColor = "#0079BF"
